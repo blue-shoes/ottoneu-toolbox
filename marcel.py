b75036988e94aecb403715ca80182cf759f444f8
Adam Scharf <adam.scharf@studsvik.com>
2020-03-06 16:33:03 -0600

    League weighted average dictionaries created

--- marcel.py
+++ marcel.py
@@ -1,14 +1,48 @@
 import pandas as pd
-def createLeagueAvgPos(dataset, y1, y2, y3):
+def createLeagueAvgPos(dataset):
     avg_dict = pd.DataFrame()
 
+    #y1 is last year, which is the last row in the table
+    y1_pa = dataset.loc[2, "PA"]
+    y2_pa = dataset.loc[1, "PA"]
+    y3_pa = dataset.loc[0, "PA"]
+
     for (cat_name, cat_data)  in dataset.iteritems():
+        if cat_name in ["PA", "Season", "G"]:
+            continue
         y1_stat = cat_data[2]
         y2_stat = cat_data[1]
         y3_stat = cat_data[0]
-        print(f"{cat_name}s - {y1_stat}, {y2_stat}, {y3_stat}")
 
-    #avg_dict["H"] = (dataset[y1, "H"] / * 5 + dataset[y2, "H"] * 4
+        if(cat_name == "AVG"):
+            avg = (3*y3_stat + 4*y2_stat + 5*y1_stat)/12
+        else:
+            avg = (3*y3_stat/y3_pa + 4*y2_stat/y2_pa + 5*y1_stat/y1_pa)/12
+
+        avg_dict[cat_name] = avg
+
+    return avg_dict
+
+def createLeagueAvgPitch(dataset):
+    avg_dict = pd.DataFrame()
+
+    y1_ip = dataset.loc[2, "IP"]
+    y2_ip = dataset.loc[1, "IP"]
+    y3_ip = dataset.loc[0, "IP"]
+
+    for (cat_name, cat_data)  in dataset.iteritems():
+        if cat_name in ["IP", "Season", "G"]:
+            continue
+        y1_stat = cat_data[2]
+        y2_stat = cat_data[1]
+        y3_stat = cat_data[0]
+
+        if cat_name == "ERA":
+            avg = (5*y3_stat + 4*y2_stat + 3*y1_stat)/12
+        else:
+            avg = (5*y3_stat/y3_ip + 4*y2_stat/y2_ip + 3*y1_stat/y1_ip)/12
+
+        avg_dict[cat_name] = avg
 
     return avg_dict
 
@@ -36,12 +70,7 @@ try:
 finally:
     driver.close()
 
-print(f"{pos_league_dataset}")
-
-pos_avg_dict = createLeagueAvgPos(pos_league_dataset, year1, year2, year3)
-
-#pos_y1_dict = pos_year1_dataset.set_index('playerid', drop=False)
-#pos_y2_dict = pos_year2_dataset.set_index('playerid', drop=False)
-#pos_y3_dict = pos_year3_dataset.set_index('playerid', drop=False)
+pos_avg_dict = createLeagueAvgPos(pos_league_dataset)
+pitch_avg_dict = createLeagueAvgPitch(pitch_league_dataset)
 
 print("We ran")
