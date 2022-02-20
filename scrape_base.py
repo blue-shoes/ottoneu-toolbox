cb59c56220408e9b33f3a50b56fbd714f24f29c2
Adam Scharf <adam.scharf@studsvik.com>
2022-02-20 09:09:27 -0500

    Adding comments

--- scrape_base.py
+++ scrape_base.py
@@ -13,6 +13,7 @@ class Scrape_Base(object):
             self.driver.close()
 
     def getDataset(self, page, element_id, filepath):
+        #Click the passed element id to download the csv file
         self.driver.get(page)
         csvJs = self.driver.find_element_by_id(element_id)
         csvJs.click()
@@ -25,10 +26,15 @@ class Scrape_Base(object):
         return self.getDatasetFromDownloads(filepath)
 
     def getDatasetFromDownloads(self, filepath):
+        #Move to Chrome downloads page and get list of download URLs
         url = self.every_downloads_chrome()
+        #Get the latest download path
         download_path = unquote(urlparse(url[0]).path)
+        #Move the file to the requested location
         shutil.move(download_path, filepath)
+        #Read the file into a DataFrame
         dataframe = pd.read_csv(filepath)
+        #Strip -1 columns that can appear for vertical spacers
         dataframe = dataframe.loc[:, ~dataframe.columns.str.startswith('-1')]
         return dataframe
 
--- scrape_fg.py
+++ scrape_fg.py
@@ -7,46 +7,57 @@ import scrape_base
 class Scrape_Fg(scrape_base.Scrape_Base):
 
     def getLeaderboardDataset(self, page, csv_name, force_download=False, player=True):
+        #Create filepath info
         subdir = f'leaderboard'
         dirname = os.path.dirname(__file__)
         subdirpath = os.path.join(dirname, subdir)
         if not path.exists(subdirpath):
             os.mkdir(subdirpath)
         filepath = os.path.join(subdirpath, csv_name)
+        #If we have the file and didn't request a redownload, just load what we have
         if path.exists(filepath) and not force_download:
             dataframe = pd.read_csv(filepath)
             dataframe = dataframe.loc[:, ~dataframe.columns.str.startswith('-1')]
         else:
+            #If we haven't used the web yet, initialize the driver
             if self.driver == None:
                 self.setupDriver()
                 self.setup_fg_login()
+            #Use driver to get dataset
             dataframe = self.getDataset(page, 'LeaderBoard1_cmdCSV', filepath, player)
+        #If the dataset is player based (not league based), reindex to the FG player id
         if player:
             dataframe.set_index("playerid", inplace=True)
             dataframe.index = dataframe.index.astype(str, copy = False)
         return dataframe
 
     def getProjectionDataset(self, page, csv_name, force_download=False, player=True):
+        #Create filepath info
         subdir = f'projection'
         dirname = os.path.dirname(__file__)
         subdirpath = os.path.join(dirname, subdir)
         if not path.exists(subdirpath):
             os.mkdir(subdirpath)
         filepath = os.path.join(subdirpath, csv_name)
+        #If we have the file and didn't request a redownload, just load what we have
         if path.exists(filepath) and not force_download:
             dataframe = pd.read_csv(filepath)
             dataframe = dataframe.loc[:, ~dataframe.columns.str.startswith('-1')]
         else:
+            #If we haven't used the web yet, initialize the driver
             if self.driver == None:
                 self.setupDriver()
                 self.setup_fg_login()
+            #Use driver to get dataset
             dataframe = self.getDataset(page, 'ProjectionBoard1_cmdCSV', filepath)
+        #If the dataset is player based (not league based), reindex to the FG player id
         if player:
             dataframe.set_index("playerid", inplace=True)
             dataframe.index = dataframe.index.astype(str, copy = False)
         return dataframe
 
     def setup_fg_login(self):
+        #FG login to get rid of ads
         self.driver.get("https://blogs.fangraphs.com/wp-login.php")
         cparser = configparser.RawConfigParser()
         cparser.read('fangraphs-config.txt')
