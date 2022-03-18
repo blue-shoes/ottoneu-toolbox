e779718b05a1654546593f1b00135c9aa7af0acd
Adam Scharf <adam.scharf@studsvik.com>
2022-03-17 20:56:08 -0400

    lot of work. best available player tables. start/stop buttons (untested), merging with salary

--- ui/draft_tool.py
+++ ui/draft_tool.py
@@ -1,3 +1,4 @@
+from re import search
 import tkinter as tk                     
 from tkinter import ttk 
 from tkinter import filedialog as fd
@@ -11,6 +12,9 @@ import util.string_util
 from scrape.scrape_ottoneu import Scrape_Ottoneu 
 
 from pathlib import Path
+import threading
+from time import sleep
+from datetime import datetime
 
 from enum import Enum
 
@@ -23,6 +27,7 @@ class IdType(Enum):
 
 class DraftTool:
     def __init__(self):
+        self.demo_source = None
         self.setup_win = tk.Tk() 
         self.value_dir = tk.StringVar()
         self.value_dir.set(Path.home())
@@ -52,16 +57,115 @@ class DraftTool:
         sv.trace("w", lambda name, index, mode, sv=sv: self.update_player_search())
         ttk.Entry(search_frame, textvariable=sv).grid(column=1,row=1)
 
+        self.start_monitor = ttk.Button(search_frame, text='Start Draft Monitor', command=self.start_draft_monitor).grid(column=0,row=2)
+        self.stop_monitor = ttk.Button(search_frame, text="Stop Draft Monitor", command=self.stop_draft_monitor).grid(column=0,row=3)
+
         f = ttk.Frame(main_frame)
         f.grid(column=1,row=1)
 
-        cols = ('Name','Value','Pos','Team','Points','P/G','P/IP')
+        cols = ('Name','Value','Salary','Pos','Team','Points','P/G','P/IP')
         self.search_view = ttk.Treeview(f, columns=cols, show='headings')    
         for col in cols:
             self.search_view.heading(col, text=col) 
         self.search_view.grid(column=0,row=0)   
 
+        tab_control = ttk.Notebook(main_frame)
+        tab_control.grid(row=2, column=0, columnspan=2)
+
+        self.pos_view = {}
+
+        overall_frame = ttk.Frame(tab_control)
+        tab_control.add(overall_frame, text='Overall')
+        self.overall_view = ttk.Treeview(overall_frame, columns=cols, show='headings')
+        for col in cols:
+            self.overall_view.heading(col, text=col)
+        self.overall_view.pack()
+        vsb = ttk.Scrollbar(overall_frame, orient="vertical", command=self.overall_view.yview)
+        vsb.pack(side='right', fill='y')
+        pos_df = self.values.loc[self.values['Salary'] == '$0']
+        for i in range(len(pos_df)):
+            name = pos_df.iloc[i, 2]
+            value = pos_df.iloc[i, 1]
+            pos = pos_df.iloc[i, 4]
+            team = pos_df.iloc[i, 3]
+            pts = "{:.1f}".format(pos_df.iloc[i, 5])
+            ppg = "{:.2f}".format(pos_df.iloc[i, 7])
+            pip = "{:.2f}".format(pos_df.iloc[i, 8])
+            self.overall_view.insert('', tk.END, values=(name, value, pos, team, pts, ppg, pip))
+
+        for pos in self.pos_values:
+            pos_frame = ttk.Frame(tab_control)
+            tab_control.add(pos_frame, text=pos) 
+            if pos in bat_pos:
+                cols = ('Name','Value','Salary','Pos','Team','Points','P/G')
+            else:
+                cols = ('Name','Value','Salary','Pos','Team','Points','P/IP')
+            self.pos_view[pos] = ttk.Treeview(pos_frame, columns=cols, show='headings')
+            for col in cols:
+                self.pos_view[pos].heading(col, text=col)
+            self.pos_view[pos].pack()
+            vsb = ttk.Scrollbar(pos_frame, orient="vertical", command=self.pos_view[pos].yview)
+            vsb.pack(side='right', fill='y')
+            pos_df = self.pos_values[pos].loc[self.pos_values[pos]['Salary'] == '$0']
+            for i in range(len(pos_df)):
+                name = pos_df.iloc[i, 2]
+                value = pos_df.iloc[i, 1]
+                position = pos_df.iloc[i, 4]
+                team = pos_df.iloc[i, 3]
+                pts = "{:.1f}".format(pos_df.iloc[i, 5])
+                rate = "{:.2f}".format(pos_df.iloc[i, 7])
+                self.pos_view[pos].insert('', tk.END, values=(name, value, position, team, pts, rate))
+
         main_frame.pack()
+
+    def start_draft_monitor(self):
+        self.run_event = threading.Event()
+        self.run_event.set()
+        t1 = threading.Thread(target = self.refresh_thread)
+        t1.start()
+        try:
+            while 1:
+                sleep(5)
+        except KeyboardInterrupt:
+            self.run_event.clear()
+            t1.join()
+    
+    def stop_draft_monitor(self):
+        self.run_event.clear()
+    
+    def refresh_thread(self):
+        last_time = datetime.now()
+        while(self.run_event.is_set()):
+            sleep(60)
+            if self.demo_source == None:
+                last_trans = Scrape_Ottoneu().scrape_recent_trans_api(self.lg_id)
+            else:
+                last_trans = pd.read_csv(self.demo_source)
+            most_recent = last_trans[0]['Date']
+            if most_recent > last_time:
+                index = len(last_trans)
+                while index >= 0:
+                    if last_trans[index]['Date'] > last_time:
+                        if last_trans[index]['Type'] == 'Add':
+                            row = []
+                            row.append(last_trans[index]['Team ID'])
+                            #team name unimportant
+                            row.append('')
+                            otto_id = last_trans[index]['Ottoneu ID']
+                            row.append(otto_id)
+                            row.append(self.positions[otto_id]['FG MajorLeagueID'])
+                            row.append(self.positions[otto_id]['FG MinorLeagueID'])
+                            #name, team, position, unimportant
+                            row.append('')
+                            row.append('')
+                            row.append('')
+                            row.append(last_trans[index]['Salary'])
+                            self.rosters.append(row, ignore_index=True)
+                        elif last_trans[index] == 'Cut':
+                            self.rosters.drop(last_trans[index]['Ottoneu ID'])
+                    index -= index
+                last_time = most_recent
+                self.refresh_views()
 
     def update_player_search(self):
         text = self.search_string.get().upper()
@@ -74,12 +178,13 @@ class DraftTool:
         for i in range(len(df)):
             name = df.iloc[i, 2]
             value = df.iloc[i, 1]
+            salary = df.iloc[i,10]
             pos = df.iloc[i, 4]
             team = df.iloc[i, 3]
             pts = "{:.1f}".format(df.iloc[i, 5])
             ppg = "{:.2f}".format(df.iloc[i, 7])
             pip = "{:.2f}".format(df.iloc[i, 8])
-            self.search_view.insert('', tk.END, values=(name, value, pos, team, pts, ppg, pip))
+            self.search_view.insert('', tk.END, values=(name, value, salary, pos, team, pts, ppg, pip))
 
     
     def create_setup_tab(self, tab):
@@ -123,12 +228,19 @@ class DraftTool:
         self.load_values()
 
         scraper = Scrape_Ottoneu()
-        positions = scraper.get_avg_salary_ds()
+        self.positions = scraper.get_avg_salary_ds()
 
         self.lg_id = self.league_num_entry.get()
         self.rosters = scraper.scrape_roster_export(self.lg_id)
 
+        self.update_rostered_players()
+
         self.setup_win.destroy()
+
+    def update_rostered_players(self):
+        self.values = self.values.merge(self.rosters[['Salary']], how='left', left_on='OttoneuID', right_index=True).fillna('$0')
+        for pos in self.pos_values:
+            self.pos_values[pos] = self.pos_values[pos].merge(self.rosters[['Salary']], how='left', left_on='OttoneuID', right_index=True).fillna('$0')
 
     def load_values(self):
         self.values = pd.read_csv(self.value_file_path)
@@ -141,11 +253,13 @@ class DraftTool:
             pos_path = os.path.join(self.value_dir.get(), f'{pos}_values.csv')
             if os.path.exists(pos_path):
                 self.pos_values[pos] = pd.read_csv(pos_path)
+                self.pos_values[pos].set_index('playerid', inplace=True)
                 #TODO data validation here
         for pos in pitch_pos:
             pos_path = os.path.join(self.value_dir.get(), f'{pos}_values.csv')
             if os.path.exists(pos_path):
                 self.pos_values[pos] = pd.read_csv(pos_path)
+                self.pos_values[pos].set_index('playerid', inplace=True)
                 #TODO data validation here
 
 def main():
