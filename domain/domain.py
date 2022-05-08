94791f73cc4663b302757e250d45858f4d68a18d
Adam Scharf <adam.scharf@studsvik.com>
2022-05-08 16:57:28 -0400

    Proof of concept ORM into database and query from db

--- domain/domain.py
+++ domain/domain.py
@@ -4,7 +4,7 @@ from sqlalchemy import Integer
 from sqlalchemy import String
 from sqlalchemy import Boolean
 from sqlalchemy import Float
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from tables import StringCol
 
@@ -13,7 +13,7 @@ Base = declarative_base()
 class Player(Base):
     __tablename__ = "player"
     ottoneu_id = Column('Ottoneu ID', Integer, primary_key=True)
-    fg_major_id = Column('FG MajorLeagueID', Integer)
+    fg_major_id = Column('FG MajorLeagueID', String)
     fg_minor_id = Column('FG MinorLeagueID', String)
     name = Column("Name",String)
     team = Column("Org",String(7))
@@ -25,7 +25,7 @@ class Player(Base):
 
 class League(Base):
     __tablename__ = "league"
-    id = Column(Integer, primary_key=True)
+    index = Column(Integer, primary_key=True)
     name = Column(String)
     format = Column(Integer)
 
@@ -33,9 +33,9 @@ class League(Base):
 
 class Team(Base):
     __tablename__ = "team"
-    id = Column(Integer, primary_key=True)
+    index = Column(Integer, primary_key=True)
 
-    league_id = Column(Integer, ForeignKey("league.id"))
+    league_id = Column(Integer, ForeignKey("league.index"))
     league = relationship("League", back_populates="teams")
 
     name = Column(String)
@@ -45,21 +45,21 @@ class Team(Base):
 
 class Roster_Spot(Base):
     __tablename__ = "roster_spot"
-    id = Column(Integer, primary_key=True)
+    index = Column(Integer, primary_key=True)
 
-    team_id = Column(Integer, ForeignKey="team.id")
+    team_id = Column(Integer, ForeignKey("team.index"))
     team = relationship("Team", back_populates="roster_spots")
 
-    ottoneu_id = Column(Integer, ForeignKey="player.ottoneu_id")
+    ottoneu_id = Column(Integer, ForeignKey("player.Ottoneu ID"))
     player = relationship("Player", back_populates="roster_spots")
 
     salary = Column(Integer)
 
 class Salary_Info(Base):
     __tablename__ = "salary_info"
-    id = Column(Integer, primary_key=True)
+    index = Column(Integer, primary_key=True)
 
-    ottoneu_id = Column("Ottoneu ID",Integer, ForeignKey="player.ottoneu_id")
+    ottoneu_id = Column("Ottoneu ID",Integer, ForeignKey("player.Ottoneu ID"))
     player = relationship("Player", back_populates="salary_info")
 
     game_type = Column(Integer)
@@ -73,9 +73,9 @@ class Salary_Info(Base):
 
 class Value(Base):
     __tablename__ = "point_value"
-    id = Column(Integer, primary_key=True)
+    index = Column(Integer, primary_key=True)
 
-    ottoneu_id = Column(Integer, ForeignKey="player.ottoneu_id")
+    ottoneu_id = Column(Integer, ForeignKey("player.Ottoneu ID"))
     player = relationship("Player", back_populates="values")
 
     value = Column(Float)
