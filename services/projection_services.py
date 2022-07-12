39b4076cbb523e279bbc54d4657b0917777f5290
Adam Scharf <adam.m.scharf@gmail.com>
2022-07-12 10:17:08 -0400

    A bunch of (untested) work to get projections into and out of the database

--- services/projection_services.py
+++ services/projection_services.py
@@ -1,9 +1,12 @@
+from pandas import DataFrame
 from domain.domain import Player, PlayerProjection, Projection, ProjectionData
 from scrape import scrape_fg
 from domain.enum import ProjectionType, StatType
 from datetime import datetime
 from services import player_services
 from dao.session import Session
+from sqlalchemy.orm import joinedload
+import pandas as pd
 
 def download_projections(projection, ros=False, dc_pt=False):
     """Returns a list of projection dataframes. Item 1 is the batting projections. Item 2 is the pitching projections"""
@@ -81,7 +84,57 @@ def convertToDcPlayingTime(proj, ros, position, fg_scraper=None):
             proj[column] = dc_proj[column]
     return proj
 
-def create_projection(type, ros=False, dc_pt=False):
+def save_projection(projection, projs):
+    with Session as session:
+        for proj in projs:
+            stat_cols = proj.columns
+            if 'IP' in stat_cols:
+                pitch = True
+            else:
+                pitch = False
+            for idx, row in proj:
+                player = player_services.get_player_by_fg_id(idx)
+                if player == None:
+                    player = player_services.create_player(idx, row)
+                player_proj = PlayerProjection()
+                player_proj.player = player
+                player_proj.projection = projection
+                player_proj.pitcher = pitch
+                projection.player_projections.append(player_proj)
+                player_proj.projection_data = []
+                for col in stat_cols:
+                    if col not in ['Name','Team','-1','playerid']:
+                        data = ProjectionData()
+                        data.player_projection = player_proj
+                        player_proj.append(data)
+                        if pitch:
+                            data.stat_type = StatType.pitch_to_enum_dict[col]
+                        else:
+                            data.stat_type = StatType.hit_to_enum_dict[col]
+                        data.stat_value = row[col]
+        session.add(projection)
+        session.commit()
+
+def create_projection_from_upload(pos_file, pitch_file, name, desc='', ros=False):
+    projection = Projection()
+    projection.type = ProjectionType.CUSTOM
+
+    projection.name = name
+    projection.calculations = []
+    projection.dc_pt = False
+    projection.detail = desc
+    projection.hide = False
+    projection.player_projections = []
+    projection.ros = ros
+    projection.timestamp = datetime.now()
+
+    pos_df = pd.read_csv(pos_file)
+    pitch_df = pd.read_csv(pitch_file)
+    # TODO: Need to confirm data matches expected format/headers/index
+
+    save_projection(projection, [pos_df, pitch_df])
+
+def create_projection_from_download(type, ros=False, dc_pt=False):
     projection = Projection()
     projection.type = type
     if ros:
@@ -102,39 +155,70 @@ def create_projection(type, ros=False, dc_pt=False):
     projection.timestamp = datetime.now()
 
     proj_type_url = ProjectionType.enum_to_url(type)
-    projs = get_projections(type, ros, dc_pt)
+    projs = download_projections(proj_type_url, ros, dc_pt)
 
-    with Session as session:
-        for proj in projs:
-            stat_cols = proj.columns
-            if 'IP' in stat_cols:
-                pitch = True
-            else:
-                pitch = False
-            for idx, row in proj:
-                player = player_services.get_player_by_fg_id(idx)
-                if player == None:
-                    player = player_services.create_player(idx, row)
-                player_proj = PlayerProjection()
-                player_proj.player = player
-                player_proj.projection = projection
-                projection.player_projections.append(player_proj)
-                player_proj.projection_data = []
-                for col in stat_cols:
-                    if col not in ['Name','Team','-1','playerid']:
-                        data = ProjectionData()
-                        data.player_projection = player_proj
-                        player_proj.append(data)
-                        if pitch:
-                            data.stat_type = StatType.pitch_to_enum_dict[col]
-                        else:
-                            data.stat_type = StatType.hit_to_enum_dict[col]
-                        data.stat_value = row[col]
-        session.add(projection)
-        session.commit()
+    save_projection(projection, projs)
 
 def get_projection_count():
     with Session as session:
         count = session.query(Projection).count()
     return count
+
+def get_projection(proj_id):
+    with Session as session:
+        proj = (session.query(Projection)
+            .options(joinedload(Projection.player_projections)
+            .options(joinedload(PlayerProjection.projection_data))
+            .options(joinedload(PlayerProjection.player)))
+            .filter_by(Projection.index == proj_id).all()
+        )
+    return proj     
+
+def convert_to_df(proj):
+    pos_col = []
+    pitch_col = []
+    #Loop to get the dataframe columns
+    for pp in proj.player_projections:
+        if len(pos_col) > 0 and len(pitch_col) > 0:
+            break
+        if pp.pitcher and len(pitch_col) == 0:
+            for pd in pp.projection_data:
+                pitch_col.append(pd.stat_type)
+        elif not pp.pitcher and len(pos_col) == 0:
+            for pd in pp.projection_data:
+                pos_col.append(pd.stat_type)
+    
+    pos_rows = []
+    pitch_rows = []
+    for pp in proj.player_projections:
+        if pp.pitcher:
+            pitch_rows.append(db_rows_to_df(pp, pitch_col))
+        else:
+            pos_rows.append(db_rows_to_df(pp, pos_col))
+    
+    pos_proj = DataFrame(pos_rows)
+    pitch_proj = DataFrame(pitch_rows)
+
+    pos_header = ['Name','Team']
+    for col in pos_col:
+        pos_header.append(StatType.enum_to_display_dict[col])
+    pitch_header = ['Name', 'Team']
+    for col in pitch_col:
+        pitch_header.append(StatType.enum_to_display_dict[col])
+    
+    pos_proj.columns = pos_header
+    pitch_proj.columns = pitch_header
+
+    return [pos_proj, pitch_proj]
+
+def db_rows_to_df(player_proj, columns):
+    row = []
+    row.append[player_proj.index]
+    row.append(player_proj.player.name)
+    row.append(player_proj.player.team)
+    for col in columns:
+        row.append(player_proj.get_stat(col))
+    return row
+
+
 
