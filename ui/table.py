f8c4588542d34738572c17be8746a8023a327aa2
Adam Scharf <adam.m.scharf@gmail.com>
2022-07-28 08:16:28 -0400

    fixes for table initialization methods

--- ui/table.py
+++ ui/table.py
@@ -8,7 +8,7 @@ from numpy import sort
 
 class Table(ttk.Treeview):
     def __init__(self, parent, columns, column_alignments=None, column_widths=None, sortable_columns=None):
-        super().__init__(parent, columns, show='headings')
+        super().__init__(parent, columns=columns, show='headings')
         self.sort_col = None
         col_num = 1
         for col in columns:
@@ -22,13 +22,13 @@ class Table(ttk.Treeview):
                     width = column_widths[col]
             self.column(f"# {col_num}",anchor=align, stretch=NO, width=width)
             if sortable_columns != None:
-                if col in sortable_columns and sortable_columns[col]:
+                if col in sortable_columns:
                     self.heading(col, text=col, command=lambda _col=col: self.sort(_col) )
                 else:
                     self.heading(col, text=col)
             else:
                 self.heading(col, text=col)
-            col += 1
+            col_num += 1
 
     
     def set_right_click_method(self, rclick_method):
