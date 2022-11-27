8de050e5cba6f17c93922c6e9ad42d1a88633f7c
blue-shoes <adam.m.scharf@gmail.com>
2022-11-27 10:48:20 -0500

    Progress dialog updates to center it in the root window

--- ui/dialog/progress.py
+++ ui/dialog/progress.py
@@ -6,8 +6,9 @@ from tkinter import messagebox as mb
 
 
 class ProgressDialog(tk.Toplevel):
-    def __init__(self, parent, title):
+    def __init__(self, parent : Toplevel, title):
         super().__init__(parent)
+        self.overrideredirect(1)
         tk.Label(self, text=title).grid(row=0,column=0)
         self.progress = 0
         self.progress_var = tk.DoubleVar()
@@ -18,6 +19,16 @@ class ProgressDialog(tk.Toplevel):
         self.progress_step = sv = tk.StringVar()
         self.progress_label = ttk.Label(self, textvariable=self.progress_step)
         self.progress_label.grid(column=0,row=2)
+
+        self.update()
+        w = self.winfo_width()
+        h = self.winfo_height()
+        parent.update()
+        x = parent.winfo_x()
+        dx = parent.winfo_width()
+        y = parent.winfo_y()
+        dy = parent.winfo_height()
+        self.geometry("+%d+%d" %(x+int(dx/2)-int(w/2), y+int(dy/2)-int(h/2)))
     
     def set_task_title(self, task_title):
         self.progress_step.set(task_title)
