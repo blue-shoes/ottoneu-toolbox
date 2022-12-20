07afc4c9a9215f343044220b181e06b6a611915f
blue-shoes <adam.m.scharf@gmail.com>
2022-12-20 11:07:41 -0500

    Handle run_event at top level in case of exception handling

--- ottoneu_tool_box.py
+++ ottoneu_tool_box.py
@@ -2,4 +2,12 @@ from ui import main
 
 if __name__ == "__main__":
     app = main.Main()
-    app.mainloop()
+    try:
+        app.mainloop()
+    except Exception:
+        if app.run_event.is_set:
+            app.run_event.clear()
+    finally:
+        if app.run_event.is_set:
+            app.run_event.clear()
+    
