4966396ce20266b996a409b1ed32d27cb307c01c
blue-shoes <adam.m.scharf@gmail.com>
2022-12-24 10:31:28 -0500

    Simplification of CalcDataType assignments in calculation_services for readability

--- services/calculation_services.py
+++ services/calculation_services.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import joinedload, load_only
 from sqlalchemy.dialects import sqlite
 from dao.session import Session
 from domain.domain import PlayerValue, ValueCalculation, Projection, PlayerProjection, Player
-from domain.enum import Position, CalculationDataType, StatType
+from domain.enum import Position, CalculationDataType as CDT, StatType, ScoringFormat, RankingBasis
 from value.point_values import PointValues
 from services import player_services, projection_services
 
@@ -21,28 +21,14 @@ def perform_point_calculation(value_calc, pd = None):
 
 def get_num_rostered_rep_levels(value_calc):
     rl_dict = {}
-    rl_dict[Position.POS_C.value] = value_calc.get_input(CalculationDataType.ROSTERED_C)
-    rl_dict[Position.POS_1B.value] = value_calc.get_input(CalculationDataType.ROSTERED_1B)
-    rl_dict[Position.POS_2B.value] = value_calc.get_input(CalculationDataType.ROSTERED_2B)
-    rl_dict[Position.POS_SS.value] = value_calc.get_input(CalculationDataType.ROSTERED_SS)
-    rl_dict[Position.POS_3B.value] = value_calc.get_input(CalculationDataType.ROSTERED_3B)
-    rl_dict[Position.POS_OF.value] = value_calc.get_input(CalculationDataType.ROSTERED_OF)
-    rl_dict[Position.POS_UTIL.value] = value_calc.get_input(CalculationDataType.ROSTERED_UTIL)
-    rl_dict[Position.POS_SP.value] = value_calc.get_input(CalculationDataType.ROSTERED_SP)
-    rl_dict[Position.POS_RP.value] = value_calc.get_input(CalculationDataType.ROSTERED_RP)
+    for pos in Position.get_discrete_offensive_pos() + Position.get_discrete_pitching_pos():
+        rl_dict[pos.value] = value_calc.get_input(CDT.pos_to_num_rostered().get(pos))
     return rl_dict
 
 def get_rep_levels(value_calc):
     rl_dict = {}
-    rl_dict[Position.POS_C.value] = value_calc.get_input(CalculationDataType.REP_LEVEL_C)
-    rl_dict[Position.POS_1B.value] = value_calc.get_input(CalculationDataType.REP_LEVEL_1B)
-    rl_dict[Position.POS_2B.value] = value_calc.get_input(CalculationDataType.REP_LEVEL_2B)
-    rl_dict[Position.POS_SS.value] = value_calc.get_input(CalculationDataType.REP_LEVEL_SS)
-    rl_dict[Position.POS_3B.value] = value_calc.get_input(CalculationDataType.REP_LEVEL_3B)
-    rl_dict[Position.POS_OF.value] = value_calc.get_input(CalculationDataType.REP_LEVEL_OF)
-    rl_dict[Position.POS_UTIL.value] = value_calc.get_input(CalculationDataType.REP_LEVEL_UTIL)
-    rl_dict[Position.POS_SP.value] = value_calc.get_input(CalculationDataType.REP_LEVEL_SP)
-    rl_dict[Position.POS_RP.value] = value_calc.get_input(CalculationDataType.REP_LEVEL_RP)
+    for pos in Position.get_discrete_offensive_pos() + Position.get_discrete_pitching_pos():
+        rl_dict[pos.value] = value_calc.get_input(CDT.pos_to_rep_level().get(pos))
     return rl_dict
 
 def save_calculation(value_calc):
