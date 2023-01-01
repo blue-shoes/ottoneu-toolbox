cd790640c2be84d756fbcf6368b84424e588781a
blue-shoes <adam.m.scharf@gmail.com>
2023-01-01 16:07:59 -0500

    Create InputException

--- domain/exception.py
+++ domain/exception.py
@@ -2,3 +2,7 @@ class FangraphsException(Exception):
     pass
 class OttoneuException(Exception):
     pass
+class InputException(Exception):
+    def __init__(self, validation_msgs, *args: object) -> None:
+        super().__init__(*args)
+        self.validation_msgs = validation_msgs
