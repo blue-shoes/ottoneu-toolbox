2beb683b3097d0d1e919e90965be9deef1ff6b4b
blue-shoes <adam.m.scharf@gmail.com>
2023-01-07 20:47:25 -0500

    #11 Reworking dialog for setting draft_target price

--- ui/dialog/draft_target.py
+++ ui/dialog/draft_target.py
@@ -4,24 +4,20 @@ from tkinter import ttk
 from tkinter import messagebox as mb
 from tkinter.messagebox import CANCEL, OK 
 
-from domain.domain import Draft_Target
 from services import player_services
 
-class Dialog(tk.TopLevel):
-    def __init__(self, parent, target:Draft_Target):
+class Dialog(tk.Toplevel):
+    def __init__(self, parent, player_id, price=None):
         super().__init__(parent)
         self.title("Set Target Price")
         frm = tk.Frame(self, borderwidth=4)
 
-        self.target = target
+        player = player_services.get_player(player_id)
 
-        if target.player is None:
-            target.player = player_services.get_player(target.player_id)
-
-        tk.Label(frm, text=f"Target Price for {target.player.name}:").grid(row=1, column=0)
+        tk.Label(frm, text=f"Target Price for {player.name}:").grid(row=1, column=0)
         self.price_tv = IntVar()
-        if target.price is not None:
-            self.price_tv.set(target.price)
+        if price is not None:
+            self.price_tv.set(price)
         tk.Entry(frm, textvariable=self.price_tv).grid(row=1, column=1)
 
         tk.Button(frm, text="OK", command=self.ok_click).grid(row=3, column=0)
@@ -31,15 +27,16 @@ class Dialog(tk.TopLevel):
 
         frm.pack()
 
-        self.protocol("WM_DELETE_WINDOW", self.cancel)
+        self.protocol("WM_DELETE_WINDOW", self.cancel_click)
 
         self.wait_window()
     
     def ok_click(self):
-        self.target.price = self.price_tv.get()
+        self.price = self.price_tv.get()
         self.status = OK
         self.destroy()
     
     def cancel_click(self):
+        self.price = None
         self.status = CANCEL
         self.destroy()
