d57a20b013ff2047a80632becdb5453cd0886c3a
blue-shoes <adam.m.scharf@gmail.com>
2023-01-28 14:57:53 -0500

    Update version to 1.1.1

--- ui/main.py
+++ ui/main.py
@@ -18,7 +18,7 @@ from domain.domain import Property
 from domain.enum import Preference as Pref, PropertyType
 from dao import db_update
    
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 
 class Main(tk.Tk):
 
