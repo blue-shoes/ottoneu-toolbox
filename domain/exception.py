26bcd8343fc166f4f1526ed405f56be5448f1ff6
blue-shoes <adam.m.scharf@gmail.com>
2023-02-10 13:28:14 -0500

    Additional documentation for custom Exception classes.

--- domain/exception.py
+++ domain/exception.py
@@ -1,4 +1,5 @@
 class InputException(Exception):
+    '''An exception for passing information about exceptions related to user UI input.'''
     def __init__(self, validation_msgs, *args: object) -> None:
         super().__init__(*args)
         self.validation_msgs = validation_msgs
--- scrape/exceptions.py
+++ scrape/exceptions.py
@@ -1,14 +1,20 @@
 class DownloadException(Exception):
+    '''Exceptions related to issues downloading files from the browser.'''
     def __init__(self, message):
         self.message = message
         super().__init__(message)
 class BrowserTypeException(Exception):
+    '''Exceptions related to the browser type used for scrapting.'''
     def __init__(self, message):
         self.message = message
         super().__init__(message)
 class FangraphsException(Exception):
+    '''Exceptions related to scraping FanGraphs data.'''
     def __init__(self, validation_msgs, *args: object) -> None:
         super().__init__(*args)
         self.validation_msgs = validation_msgs
 class OttoneuException(Exception):
-    pass
+    '''Exceptions related to scraping Ottoneu data.'''
+    def __init__(self, validation_msgs, *args: object) -> None:
+        super().__init__(*args)
+        self.validation_msgs = validation_msgs
