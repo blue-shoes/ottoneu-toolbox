d7868cfbed25826436aa8755f2ecc36a736a16fb
blue-shoes <adam.m.scharf@gmail.com>
2023-03-13 08:50:37 -0400

    #42. Move all domain classes to @dataclass

--- dao/session.py
+++ dao/session.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import  sessionmaker
 import os
-from domain.domain import Base
+from domain.domain import mapper_registry
 
 db_dir = 'db'
 if not os.path.exists(db_dir):
     os.mkdir(db_dir)
 db_loc = os.path.join('db', 'otto_toolbox.db')
 engine = create_engine(f"sqlite:///{db_loc}")
-Base.metadata.create_all(engine)
+mapper_registry.generate_base().metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
