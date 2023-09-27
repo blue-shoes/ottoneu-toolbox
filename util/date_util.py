0bc027dd8fe08274eae1849790eefad448639920
blue-shoes <adam.m.scharf@gmail.com>
2023-09-27 09:34:18 -0400

    Creation of date_util.is_offseason method

--- util/date_util.py
+++ util/date_util.py
@@ -7,3 +7,12 @@ def get_current_ottoneu_year():
         return now.year + 1
     else:
         return now.year
+
+def is_offseason():
+    '''Returns if it is currently the offseason. Offseason is defined as October through January.'''
+    now = datetime.now()
+    if now.month > 9:
+        return True
+    if now.month < 2:
+        return True
+    return False
