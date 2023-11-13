f128cfd15f650f057019889d05cdcf59ede111ec
blue-shoes <adam.m.scharf@gmail.com>
2023-11-13 13:01:33 -0500

    Creation of wizard and dialog selection components for CustomScoring formats

---
+++ ui/dialog/format_select.py
@@ -0,0 +1,124 @@
+import tkinter as tk     
+from tkinter import *              
+from tkinter import ttk 
+from tkinter import messagebox as mb
+
+from ui.dialog import progress
+from ui.dialog.wizard import custom_scoring
+from ui.table.table import Table, bool_to_table
+from domain.domain import CustomScoring
+
+from services import custom_scoring_services
+
+class Dialog(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.parent = parent
+        self.scoring = None
+        self.deleted_format_ids = []
+        self.title("Select a Scoring Format")
+        frm = tk.Frame(self, borderwidth=4)
+
+        top_frm = tk.Frame(frm)
+        top_frm.grid(row=0, sticky=tk.E)
+
+        self.format_list = custom_scoring_services.get_all_formats()
+
+        cols = ('Name', 'Detail', 'Points?')
+        align = {}
+        align['Name'] = W
+        align['Detail'] = W
+        width = {}
+        width['Name'] = 150
+        width['Detail'] = 250
+        width['Points'] = 40
+
+        self.format_table = pt = Table(frm, cols, align, width, cols)
+        pt.grid(row=0)
+        pt.set_row_select_method(self.on_select)
+        pt.set_refresh_method(self.populate_table)
+        pt.set_double_click_method(self.double_click)
+        pt.set_right_click_method(self.rclick)
+
+        self.populate_table()
+
+        bot_frm = tk.Frame(frm)
+        bot_frm.grid(row=1, sticky=tk.E)
+
+        ttk.Button(bot_frm, text="OK", command=self.set_format).grid(row=0, column=0)
+        ttk.Button(bot_frm, text="Cancel", command=self.cancel).grid(row=0, column=1)
+        ttk.Button(bot_frm, text="Create New...", command=self.create_scoring_format).grid(row=0,column=2)
+
+        frm.pack()
+
+        self.protocol("WM_DELETE_WINDOW", self.cancel)
+
+        self.focus_force()
+
+        self.wait_window()
+    
+    def create_scoring_format(self):
+        dialog = custom_scoring.Dialog(self.master)
+        if dialog.scoring is not None:
+            self.scoring = dialog.scoring
+            self.destroy()
+        else:
+            self.lift()
+            self.focus_force()
+    
+    def populate_table(self):
+        for scoring_format in self.format_list:
+            self.format_table.insert('', tk.END, text=str(scoring_format.id), values=(scoring_format.name, scoring_format.description, bool_to_table(scoring_format.points_format)))
+
+    def double_click(self, event):
+        self.on_select(event)
+        self.set_format()
+
+    def on_select(self, event:Event):
+        if len(event.widget.selection()) > 0:
+            selection = event.widget.item(event.widget.selection()[0])["text"]
+            for scoring_format in self.format_list:
+                if scoring_format.id == int(selection):
+                    self.scoring = scoring_format
+                    break
+        else:
+            self.scoring = None
+    
+    def rclick(self, event):
+        iid = event.widget.identify_row(event.y)
+        event.widget.selection_set(iid)
+        scoring_format_id = int(event.widget.item(event.widget.selection()[0])["text"])
+        popup = tk.Menu(self.parent, tearoff=0)
+        popup.add_command(label="Delete", command=lambda: self.delete_format(scoring_format_id))
+        try:
+            popup.post(event.x_root, event.y_root)
+        finally:
+            popup.grab_release()
+    
+    def delete_format(self, format_id:int):
+        for scoring_format in self.format_list:
+            if scoring_format.id == format_id:
+                sf = scoring_format
+                break
+        if mb.askokcancel('Delete Scoring Format', f'Confirm deletion of scoring format {sf.name}'):
+            self.perform_deletion(scoring_format)
+        else:
+            self.lift()
+            self.focus_force()
+
+    def perform_deletion(self, scoring_format:CustomScoring):
+        self.lift()
+        pd = progress.ProgressDialog(self.parent, 'Deleting Scoring Format')
+        pd.set_completion_percent(15)
+        custom_scoring_services.delete_by_id(scoring_format.id)
+        self.format_list.remove(scoring_format)
+        self.format_table.refresh()
+        self.deleted_format_ids.append(scoring_format.id)
+        pd.complete()
+
+    def cancel(self):
+        self.scoring = None
+        self.destroy()
+    
+    def set_format(self):
+        self.destroy()
---
+++ ui/dialog/wizard/custom_scoring.py
@@ -0,0 +1,290 @@
+import tkinter as tk     
+from tkinter import *              
+from tkinter import ttk 
+
+from domain.domain import CustomScoring, CustomScoringCategory
+from domain.enum import StatType
+from services import custom_scoring_services
+from ui.dialog.wizard import wizard
+
+class Dialog(wizard.Dialog):
+
+    scoring:CustomScoring
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    def setup(self):
+        self.wizard = Wizard(self)
+        self.wizard.pack()
+        self.title('Create Custom Scoring Format')
+        self.scoring = None
+
+        return self.wizard
+
+class Wizard(wizard.Wizard):
+    def __init__(self, parent:Dialog):
+        super().__init__(parent)
+        self.parent = parent
+        self.points_format = False
+        self.steps.append(Step0(self))
+        self.steps.append(Hitter_Cats(self))
+        self.steps.append(Hitter_Points(self))
+        self.steps.append(Pitcher_Cats(self))
+        self.steps.append(Pitcher_Points(self))
+        #self.steps.append(Confirm(self))
+
+        self.show_step(0)
+    
+    def cancel(self):
+        self.parent.scoring = None
+        super().cancel()
+    
+    def determine_next_step(self):
+        if self.points_format:
+            if self.current_step == 0 or self.current_step == 2 :
+                return self.current_step + 2
+        elif self.current_step == 1 or self.current_step == 3: 
+            return self.current_step + 2
+        return super().determine_next_step()
+    
+    def determine_previous_step(self):
+        if self.points_format:
+            if self.current_step == 4 or self.current_step == 2:
+                return self.current_step - 2
+        elif self.current_step == 5 or self.current_step == 3:
+            return self.current_step - 2
+        return super().determine_previous_step()
+    
+    def is_last_page(self, step):
+        if self.points_format:
+            return step == len(self.steps)-1
+        return step == len(self.steps)-2
+
+    def finish(self):
+        self.steps[self.current_step].validate()
+        self.parent.scoring = custom_scoring_services.save_scoring_format(self.parent.scoring)
+        super().finish()
+
+class Step0(tk.Frame):
+    def __init__(self, parent:Wizard):
+        super().__init__(parent)
+        self.parent = parent
+        header = tk.Label(self, text="Initialize Scoring Format")
+        header.grid(row=0, column=0, columnspan=2)
+
+        tk.Label(self, text="Name:").grid(row=1, column=0)
+        self.name_tv = StringVar()
+        tk.Entry(self, textvariable=self.name_tv).grid(row=1, column=1)
+
+        tk.Label(self, text="Description:").grid(row=2, column=0)
+        self.desc_tv = StringVar()
+        tk.Entry(self, textvariable=self.desc_tv).grid(row=2, column=1)
+
+        self.scoring_basis_iv = IntVar()
+        self.scoring_basis_iv.set(0)
+        tk.Radiobutton(self, text='Categories', variable=self.scoring_basis_iv, value=0).grid(row=3, column=0)
+        tk.Radiobutton(self, text='Points', variable=self.scoring_basis_iv, value=1).grid(row=3, column=1)
+
+    def on_show(self):
+        return True
+    
+    def validate(self):
+        self.parent.validate_msg = ''
+        if self.name_tv.get() is None or self.name_tv.get() == '':
+            self.parent.validate_msg = 'Please input a name'
+            return False
+        self.parent.parent.scoring = CustomScoring()
+        self.parent.parent.scoring.name = self.name_tv.get()
+        self.parent.parent.scoring.description = self.desc_tv.get()
+        self.parent.parent.scoring.points_format = (self.scoring_basis_iv.get() == 1)
+        self.parent.points_format = self.parent.parent.scoring.points_format
+        return True
+
+class Hitter_Cats(tk.Frame):
+    def __init__(self, parent:Wizard):
+        super().__init__(parent)
+        self.parent = parent
+        tk.Label(self, text="Select Hitter Categories").grid(row=0, column=0, columnspan=2)
+        self.stat_vars = {}
+        
+        for idx, stat in enumerate(StatType.get_all_hit_stattype()):
+            self.stat_vars[stat] = BooleanVar()
+            tk.Checkbutton(self, text = stat.display, variable=self.stat_vars[stat], justify=LEFT, anchor=W).grid(sticky=W, row = (int)(idx/2)+1, column=idx % 2)
+    
+    def on_show(self):
+        for stat, bv in self.stat_vars.items():
+            if stat in [s.category for s in self.parent.parent.scoring.stats]:
+                bv.set(True)
+            else:
+                bv.set(False)
+        return True
+    
+    def validate(self):
+        if self.parent.parent.scoring.stats is None:
+            self.parent.parent.scoring.stats = []
+        to_remove = []
+        for csc in self.parent.parent.scoring.stats:
+            if csc.category.hitter:
+                to_remove.append(csc)
+        self.parent.parent.scoring.stats = [s for s in self.parent.parent.scoring.stats if s not in to_remove]
+        found_cat = False
+        for stat, bv in self.stat_vars.items():
+            if bv.get():
+                csc = CustomScoringCategory()
+                csc.category = stat
+                self.parent.parent.scoring.stats.append(csc)
+                found_cat = True
+        if found_cat:
+            return True
+        self.parent.validate_msg = 'Please select at least one stat category'
+        return False
+
+class Hitter_Points(tk.Frame):
+    def __init__(self, parent:Wizard):
+        super().__init__(parent)
+        self.parent = parent
+        tk.Label(self, text="Select Hitter Categories").grid(row=0, column=0, columnspan=2)
+        self.stat_vars = {}
+        self.stat_points = {}
+        
+        for idx, stat in enumerate(StatType.get_all_hit_stattype()):
+            self.stat_vars[stat] = BooleanVar()
+            tk.Checkbutton(self, text = stat.display, variable=self.stat_vars[stat], command=lambda _stat=stat: self.toggle_stat(_stat), justify=LEFT, anchor=W).grid(sticky=W, row = (int)(idx/2)+1, column=2*(idx % 2))
+            self.stat_points[stat] = sp = tk.Entry(self)
+            sp.grid(row = (int)(idx/2)+1, column=2*(idx % 2)+1)
+
+    def toggle_stat(self, stat:StatType):
+        if self.stat_vars[stat].get():
+            self.stat_points[stat].configure(state='active')
+        else:
+            self.stat_points[stat].configure(state='disable')
+
+    def on_show(self):
+        for stat, bv in self.stat_vars.items():
+            if stat in [s.category for s in self.parent.parent.scoring.stats]:
+                bv.set(True)
+                self.stat_points[stat].configure(state='active')
+            else:
+                bv.set(False)
+                self.stat_points[stat].configure(state='disable')
+        return True
+    
+    def validate(self):
+        if self.parent.parent.scoring.stats is None:
+            self.parent.parent.scoring.stats = []
+        to_remove = []
+        for csc in self.parent.parent.scoring.stats:
+            if csc.category.hitter:
+                to_remove.append(csc)
+        self.parent.parent.scoring.stats = [s for s in self.parent.parent.scoring.stats if s not in to_remove]
+        found_cat = False
+        for stat, bv in self.stat_vars.items():
+            if bv.get():
+                csc = CustomScoringCategory()
+                csc.category = stat
+                try:
+                    csc.points = float(self.stat_points[stat])
+                except ValueError:
+                    self.parent.validate_msg = 'Point entries must be numbers'
+                    return False
+                self.parent.parent.scoring.stats.append(csc)
+                found_cat = True
+        if found_cat:
+            return True
+        self.parent.validate_msg = 'Please select at least one stat category'
+        return False
+        
+class Pitcher_Cats(tk.Frame):
+    def __init__(self, parent:Wizard):
+        super().__init__(parent)
+        self.parent = parent
+        tk.Label(self, text="Select Pitcher Categories").grid(row=0, column=0, columnspan=2)
+        self.stat_vars = {}
+        
+        for idx, stat in enumerate(StatType.get_all_pitch_stattype(no_rates=False)):
+            self.stat_vars[stat] = BooleanVar()
+            tk.Checkbutton(self, text = stat.display, variable=self.stat_vars[stat], justify=LEFT, anchor=W).grid(sticky=W, row = (int)(idx/2)+1, column=idx % 2)
+    
+    def on_show(self):
+        for stat, bv in self.stat_vars.items():
+            if stat in [s.category for s in self.parent.parent.scoring.stats]:
+                bv.set(True)
+            else:
+                bv.set(False)
+        return True
+    
+    def validate(self):
+        if self.parent.parent.scoring.stats is None:
+            self.parent.parent.scoring.stats = []
+        to_remove = []
+        for csc in self.parent.parent.scoring.stats:
+            if not csc.category.hitter:
+                to_remove.append(csc)
+        self.parent.parent.scoring.stats = [s for s in self.parent.parent.scoring.stats if s not in to_remove]
+        found_cat = False
+        for stat, bv in self.stat_vars.items():
+            if bv.get():
+                csc = CustomScoringCategory()
+                csc.category = stat
+                self.parent.parent.scoring.stats.append(csc)
+                found_cat = True
+        if found_cat:
+            return True
+        self.parent.validate_msg = 'Please select at least one stat category'
+        return False      
+
+class Pitcher_Points(tk.Frame):
+    def __init__(self, parent:Wizard):
+        super().__init__(parent)
+        self.parent = parent
+        tk.Label(self, text="Select Pitcher Categories").grid(row=0, column=0, columnspan=2)
+        self.stat_vars = {}
+        self.stat_points = {}
+        
+        for idx, stat in enumerate(StatType.get_all_pitch_stattype(no_rates=True)):
+            self.stat_vars[stat] = BooleanVar()
+            tk.Checkbutton(self, text = stat.display, variable=self.stat_vars[stat], command=lambda _stat=stat: self.toggle_stat(_stat), justify=LEFT, anchor=W).grid(sticky=W, row = (int)(idx/2)+1, column=2*(idx % 2))
+            self.stat_points[stat] = sp = tk.Entry(self)
+            sp.grid(row = (int)(idx/2)+1, column=2*(idx % 2)+1)
+    
+    def toggle_stat(self, stat:StatType):
+        if self.stat_vars[stat].get():
+            self.stat_points[stat].configure(state='active')
+        else:
+            self.stat_points[stat].configure(state='disable')
+
+    def on_show(self):
+        for stat, bv in self.stat_vars.items():
+            if stat in [s.category for s in self.parent.parent.scoring.stats]:
+                bv.set(True)
+                self.stat_points[stat].configure(state='active')
+            else:
+                bv.set(False)
+                self.stat_points[stat].configure(state='disable')
+        return True
+    
+    def validate(self):
+        if self.parent.parent.scoring.stats is None:
+            self.parent.parent.scoring.stats = []
+        to_remove = []
+        for csc in self.parent.parent.scoring.stats:
+            if not csc.category.hitter:
+                to_remove.append(csc)
+        self.parent.parent.scoring.stats = [s for s in self.parent.parent.scoring.stats if s not in to_remove]
+        found_cat = False
+        for stat, bv in self.stat_vars.items():
+            if bv.get():
+                csc = CustomScoringCategory()
+                csc.category = stat
+                try:
+                    csc.points = float(self.stat_points[stat])
+                except ValueError:
+                    self.parent.validate_msg = 'Point entries must be numbers'
+                    return False
+                self.parent.parent.scoring.stats.append(csc)
+                found_cat = True
+        if found_cat:
+            return True
+        self.parent.validate_msg = 'Please select at least one stat category'
+        return False
