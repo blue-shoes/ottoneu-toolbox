9de21d37959534291cb4c1582d36f1ec14c7feca
blue-shoes <adam.m.scharf@gmail.com>
2023-12-21 14:55:44 -0500

    Creation of wizard and dialog for non-Ottoneu starting positions

---
+++ ui/dialog/starting_select.py
@@ -0,0 +1,124 @@
+import tkinter as tk     
+from tkinter import *              
+from tkinter import ttk 
+from tkinter import messagebox as mb
+
+from ui.dialog import progress
+from ui.dialog.wizard import starting_position
+from ui.table.table import Table, bool_to_table
+from domain.domain import StartingPositionSet
+
+from services import starting_positions_services
+
+class Dialog(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.parent = parent
+        self.starting_set = None
+        self.deleted_starting_set_ids = []
+        self.title("Select a Starting Position Set")
+        frm = tk.Frame(self, borderwidth=4)
+
+        top_frm = tk.Frame(frm)
+        top_frm.grid(row=0, sticky=tk.E)
+
+        self.starting_set_list = starting_positions_services.get_all_starting_sets()
+
+        cols = ('Name', 'Detail')
+        align = {}
+        align['Name'] = W
+        align['Detail'] = W
+        width = {}
+        width['Name'] = 150
+        width['Detail'] = 250
+
+        self.starting_set_table = sst = Table(frm, cols, align, width, cols)
+        sst.grid(row=0)
+        sst.set_row_select_method(self.on_select)
+        sst.set_refresh_method(self.populate_table)
+        sst.set_double_click_method(self.double_click)
+        sst.set_right_click_method(self.rclick)
+
+        self.populate_table()
+
+        bot_frm = tk.Frame(frm)
+        bot_frm.grid(row=1, sticky=tk.E)
+
+        ttk.Button(bot_frm, text="OK", command=self.set_starting_set).grid(row=0, column=0)
+        ttk.Button(bot_frm, text="Cancel", command=self.cancel).grid(row=0, column=1)
+        ttk.Button(bot_frm, text="Create New...", command=self.create_starting_set).grid(row=0,column=2)
+
+        frm.pack()
+
+        self.protocol("WM_DELETE_WINDOW", self.cancel)
+
+        self.focus_force()
+
+        self.wait_window()
+    
+    def create_starting_set(self):
+        dialog = starting_position.Dialog(self.master)
+        if dialog.starting_set is not None:
+            self.starting_set = dialog.starting_set
+            self.destroy()
+        else:
+            self.lift()
+            self.focus_force()
+    
+    def populate_table(self):
+        self.starting_set_table.insert('', tk.END, text=str(-1), values=('Ottoneu', 'Ottoneu Defaults'))
+        for starting_set in self.starting_set_list:
+            self.starting_set_table.insert('', tk.END, text=str(starting_set.id), values=(starting_set.name, starting_set.detail))
+
+    def double_click(self, event):
+        self.on_select(event)
+        self.set_starting_set()
+
+    def on_select(self, event:Event):
+        if len(event.widget.selection()) > 0:
+            selection = event.widget.item(event.widget.selection()[0])["text"]
+            for starting_set in self.starting_set_list:
+                if starting_set.id == int(selection):
+                    self.starting_set = starting_set
+                    break
+        else:
+            self.starting_set = None
+    
+    def rclick(self, event):
+        iid = event.widget.identify_row(event.y)
+        event.widget.selection_set(iid)
+        starting_set_id = int(event.widget.item(event.widget.selection()[0])["text"])
+        popup = tk.Menu(self.parent, tearoff=0)
+        popup.add_command(label="Delete", command=lambda: self.delete_starting_set(starting_set_id))
+        try:
+            popup.post(event.x_root, event.y_root)
+        finally:
+            popup.grab_release()
+    
+    def delete_starting_set(self, starting_set_id:int):
+        for starting_set in self.starting_set_list:
+            if starting_set.id == starting_set_id:
+                sf = starting_set
+                break
+        if mb.askokcancel('Delete Starting Position Set', f'Confirm deletion of starting set {sf.name}'):
+            self.perform_deletion(starting_set)
+        else:
+            self.lift()
+            self.focus_force()
+
+    def perform_deletion(self, starting_set:StartingPositionSet):
+        self.lift()
+        pd = progress.ProgressDialog(self.parent, 'Deleting Starting Position Set')
+        pd.set_completion_percent(15)
+        starting_positions_services.delete_by_id(starting_set.id)
+        self.starting_set_list.remove(starting_set)
+        self.starting_set_table.refresh()
+        self.deleted_starting_set_ids.append(starting_set.id)
+        pd.complete()
+
+    def cancel(self):
+        self.starting_set = None
+        self.destroy()
+    
+    def set_starting_set(self):
+        self.destroy()
---
+++ ui/dialog/wizard/starting_position.py
@@ -0,0 +1,121 @@
+import tkinter as tk     
+from tkinter import *              
+from tkinter import ttk 
+
+from domain.domain import StartingPosition, StartingPositionSet
+from domain.enum import Position
+from services import starting_positions_services
+from ui.dialog.wizard import wizard
+
+class Dialog(wizard.Dialog):
+
+    starting_set:StartingPositionSet
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    def setup(self):
+        self.wizard = Wizard(self)
+        self.wizard.pack()
+        self.title('Create Starting Position Set')
+        self.starting_set = None
+
+        return self.wizard
+
+class Wizard(wizard.Wizard):
+    def __init__(self, parent:Dialog):
+        super().__init__(parent)
+        self.parent = parent
+        self.points_format = False
+        self.steps.append(Step0(self))
+        self.steps.append(Positions(self))
+
+        self.show_step(0)
+    
+    def cancel(self):
+        self.parent.starting_set = None
+        super().cancel()
+
+    def finish(self):
+        self.steps[self.current_step].validate()
+        self.parent.starting_set = starting_positions_services.save_starting_position_set(self.parent.starting_set)
+        super().finish()
+
+class Step0(tk.Frame):
+    def __init__(self, parent:Wizard):
+        super().__init__(parent)
+        self.parent = parent
+        header = tk.Label(self, text="Initialize Starting Position Set")
+        header.grid(row=0, column=0, columnspan=2)
+
+        tk.Label(self, text="Name:").grid(row=1, column=0)
+        self.name_tv = StringVar()
+        tk.Entry(self, textvariable=self.name_tv).grid(row=1, column=1)
+
+        tk.Label(self, text="Description:").grid(row=2, column=0)
+        self.desc_tv = StringVar()
+        tk.Entry(self, textvariable=self.desc_tv).grid(row=2, column=1)
+
+    def on_show(self):
+        return True
+    
+    def validate(self):
+        self.parent.validate_msg = ''
+        if self.name_tv.get() is None or self.name_tv.get() == '':
+            self.parent.validate_msg = 'Please input a name'
+            return False
+        self.parent.parent.starting_set = StartingPositionSet()
+        self.parent.parent.starting_set.name = self.name_tv.get()
+        self.parent.parent.starting_set.detail = self.desc_tv.get()
+        return True
+
+class Positions(tk.Frame):
+    def __init__(self, parent:Wizard):
+        super().__init__(parent)
+        self.parent = parent
+        tk.Label(self, text="Select Positions and Counts").grid(row=0, column=0, columnspan=2)
+        self.positions = {}
+        self.count_entry = {}
+        self.counts = {}
+        
+        for idx, pos in enumerate(Position.get_all_offensive_pos() + Position.get_discrete_pitching_pos() + [Position.POS_P]):
+            self.positions[pos] = BooleanVar()
+            tk.Checkbutton(self, text = pos.value, variable=self.positions[pos], command=lambda _pos=pos: self.toggle_pos(_pos), justify=LEFT, anchor=W).grid(sticky=W, row = (int)(idx/2)+1, column=2*(idx % 2))
+            count = StringVar()
+            self.counts[pos] = count
+            self.count_entry[pos] = ce = tk.Entry(self, textvariable=count)
+            ce.grid(row = (int)(idx/2)+1, column=2*(idx % 2)+1)
+
+    def toggle_pos(self, pos:Position):
+        if self.positions[pos].get():
+            self.count_entry[pos].configure(state='normal')
+        else:
+            self.count_entry[pos].configure(state='disabled')
+
+    def on_show(self):
+        for pos, bv in self.positions.items():
+            if pos in [s.position for s in self.parent.parent.starting_set.positions]:
+                bv.set(True)
+                self.count_entry[pos].configure(state='active')
+            else:
+                bv.set(False)
+                self.count_entry[pos].configure(state='disable')
+        return True
+    
+    def validate(self):
+        self.parent.parent.starting_set.positions = []
+        found_pos = False
+        for pos, bv in self.positions.items():
+            if bv.get():
+                sp = StartingPosition(position=pos)
+                try:
+                    sp.count = int(self.counts[pos].get())
+                except ValueError:
+                    self.parent.validate_msg = 'Point entries must be numbers'
+                    return False
+                self.parent.parent.starting_set.positions.append(sp)
+                found_pos = True
+        if found_pos:
+            return True
+        self.parent.validate_msg = 'Please select at least one position'
+        return False
